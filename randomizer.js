4cf685025a79424ef3cc1b2cff1bb3c9e06799fa
Dmitry <deloakk.mr@mail.ru>
2018-12-27 12:53:43 +0300

    Небольшое исправление

--- randomizer.js
+++ randomizer.js
@@ -1,7 +1,7 @@
 (function () {
     window.randomizer = {
         GenerateRandomNumber: function (min, max) {
-            var random = Math.floor((Math.random * max) + min);
+            var random = Math.floor((Math.random() * max) + min);
             
             if (random > max) {
                 random = random - min;
