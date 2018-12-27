8006d6d971f1d07cf1d0140941cf788257ee5732
Dmitry <deloakk.mr@mail.ru>
2018-12-27 16:48:30 +0300

    Написал функцию получения случайного цвета

--- js/data.js
+++ js/data.js
@@ -1,3 +1,22 @@
 (function () {
+    var colors = [
+        '#263345',
+        '#ff7800',
+        '#2b81ee',
+        '#00c3f3',
+        '#b31b42',
+        '#47ffaf',
+        '#e3e834',
+        '#ffa246',
+        '#e834b1',
+        '#3a55ff'
+    ];
     
+    window.data = {
+        GetColor: function () {
+            var random = window.randomizer.GenerateRandomNumber(0, colors.length);
+            
+            return colors[random];
+        }
+    };
 })();
