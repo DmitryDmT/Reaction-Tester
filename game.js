a3d30884536dbfd167337621a13b6e1ef0ba099e
Dmitry <deloakk.mr@mail.ru>
2018-12-27 10:45:41 +0300

    Выделил в отдельный модуль

---
+++ game.js
@@ -0,0 +1,55 @@
+(function () {
+    var startButton = document.querySelector('.start-button');
+    var field = document.querySelector('.field');
+    var figuresContainer = document.querySelector('.figures-container');
+
+    field.style.width = (window.innerWidth - 150) + 'px';
+
+    var figureValue = null;
+
+    var DisableStartButton = function () {
+        startButton.classList.remove('start-button--hover-blue');
+        startButton.style.opacity = 0.3;
+        startButton.style.cursor = 'default';
+        startButton.disabled = true;
+    };
+
+    var OnFiguresClick = function (evt) {
+        figureValue = evt.target.value;
+
+        if (figureValue !== undefined) {
+            return figureValue;
+        }
+    };
+
+    var OnStartClick = function (evt) {
+        var windowWidth = window.innerWidth - 150;
+
+        if (field.style.width > windowWidth + 'px' || field.style.width < windowWidth + 'px') {
+            alert("Поле слишком маленькое / большое. Сейчас перезагрузится страница.");
+            window.location.reload();
+        }
+
+        if (!figureValue) {
+            alert("Незабудь выбрать фигуру");
+        }
+        else {
+            DisableStartButton();
+
+            var figure = document.createElement('div');
+            figure.classList.add(figureValue);
+            figure.style.top = '0';
+            figure.style.left = '0';
+            field.appendChild(figure);
+
+            field.addEventListener('click', function (evt) {
+
+            });
+
+            startButton.removeEventListener('click', OnStartClick);
+        }
+    };
+
+    figuresContainer.addEventListener('click', OnFiguresClick);
+    startButton.addEventListener('click', OnStartClick);
+})();
